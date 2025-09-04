bdb26b51fff5f82244bf084cf4a7abec22deb823
Michał Pasynek <terabajt@terabajtlw.pl>
2025-09-04 09:12:24 +0200

    init commit

---
+++ src/test/java/pl/fixpoint/fixpointbackend/FixpointBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fixpoint.fixpointbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixpointBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
